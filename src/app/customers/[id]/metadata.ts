29d040a25d2dbbc6fd2409dd0162d06b5fc69d98
Tee <teeramail@gmail.com>
2025-04-29 09:35:24 +0700

    Describe what you changed

--- src/app/customers/[id]/metadata.ts
+++ src/app/customers/[id]/metadata.ts
@@ -1,4 +1,4 @@
-import { Metadata } from "next";
+import type { Metadata } from "next";
 
 // Define dynamic metadata for the customer detail page
 export const generateMetadata = async ({ params }: { params: { id: string } }): Promise<Metadata> => {
