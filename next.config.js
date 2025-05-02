c99925b4df957e9871b5197a0a28d6aa7427bdf8
Teera <teeramail@gmail.com>
2025-05-02 14:41:52 +0700

    initial

--- next.config.js
+++ next.config.js
@@ -42,6 +42,13 @@ const config = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default config;
